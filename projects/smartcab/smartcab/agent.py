49f681373e54bfdc5449058a4f71b60ab1097d0e
Yi Zhao <yi@yizhao.me>
2017-02-16 16:59:44 -0500

    second smartcab submission

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -205,7 +205,7 @@ def run():
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test=20, tolerance=0.01)
+    sim.run(n_test=20, tolerance=0.005)
 
 
 if __name__ == '__main__':
